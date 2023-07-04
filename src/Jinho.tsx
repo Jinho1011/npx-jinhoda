762204716a1b40ee9bc50b285004e8289e01482a
Jeon <gino9940@gmail.com>
2023-07-04 15:44:50 +0900

    feat: implement Jinho introduce

--- src/Jinho.tsx
+++ src/Jinho.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React from 'react';
 // @ts-ignore
-import { Box, Text } from "ink";
+import { Box, Text } from 'ink';
 
 export const Jinho = () => {
   return (
-    <Box margin={2} flexDirection="column">
-      <Text color="#A9D0F5">안녕하세요</Text>
+    <Box
+      width={'28'}
+      paddingTop={1}
+      paddingBottom={1}
+      paddingLeft={2}
+      paddingRight={2}
+      margin={2}
+      flexDirection="column"
+      borderColor={'#EEEEEE'}
+      borderStyle={'round'}
+    >
+      <Box marginBottom={1} alignItems="center">
+        <Text bold color={'#EEEEEE'}>
+          Hello there 👋
+        </Text>
+      </Box>
+      <Box marginBottom={1} alignItems="center">
+        <Text color={'#EEEEEE'}>
+          I'm{' '}
+          <Text color={'#068FFF'} bold>
+            Jeon Jinho
+          </Text>
+          , a Frontend Developer that loves to code and build products with a
+          delightful user experience.
+        </Text>
+      </Box>
+      <Box alignItems="center">
+        <Text color={'#EEEEEE'}>
+          Checkout my github!{' '}
+          <Text underline>https://github.com/Jinho1011</Text>
+        </Text>
+      </Box>
     </Box>
   );
 };
